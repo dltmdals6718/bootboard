ea0aba8f6079a5ea2672309dde3a99e12d13212a
LSM <dltmdals6718@naver.com>
2023-07-15 17:20:40 +0900

    게시글 검색 완료

--- src/main/java/com/example/board/controller/CommentController.java
+++ src/main/java/com/example/board/controller/CommentController.java
@@ -6,6 +6,7 @@ import com.example.board.service.CommentService;
 import com.example.board.service.PosterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- src/main/java/com/example/board/repository/SpringDataJpaPosterRepository.java
+++ src/main/java/com/example/board/repository/SpringDataJpaPosterRepository.java
@@ -9,6 +9,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface SpringDataJpaPosterRepository extends JpaRepository<Poster, Long> {
-
     Page<Poster> findByTitleContaining(String title, Pageable pageable);
 }
