096c75b4b084cf981ad2abde947594efd831a1bb
LSM <dltmdals6718@naver.com>
2023-08-14 18:56:06 +0900

    카테고리 추가에 따른 메서드 추가

--- src/main/java/com/example/board/repository/SpringDataJpaPosterRepository.java
+++ src/main/java/com/example/board/repository/SpringDataJpaPosterRepository.java
@@ -1,5 +1,6 @@
 package com.example.board.repository;
 
+import com.example.board.domain.Category;
 import com.example.board.domain.Poster;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
@@ -10,4 +11,8 @@ import java.util.Optional;
 
 public interface SpringDataJpaPosterRepository extends JpaRepository<Poster, Long> {
     Page<Poster> findByTitleContaining(String title, Pageable pageable);
+
+    Page<Poster> findByCategoryAndTitleContaining(Category category, String title, Pageable pageable);
+    // Page<Poster> findByTitleContainingAndCategory(Category category, String title, Pageable pageable); 순서 주의!
+    Page<Poster> findByCategory(Category category, Pageable pageable);
 }
