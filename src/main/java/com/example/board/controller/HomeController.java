09ac2b77f83d585e6cf18ed1d796bf41c85cd849
LSM <dltmdals6718@naver.com>
2023-09-27 12:40:32 +0900

    로그인 여부에 따라 헤더(네비게이션바) 달리하기

--- src/main/java/com/example/board/controller/HomeController.java
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -1,13 +1,30 @@
 package com.example.board.controller;
 
+import com.example.board.domain.Member;
+import com.example.board.domain.SessionConst;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            model.addAttribute("isLogin", false);
+        }
+        else {
+            model.addAttribute("isLogin", true);
+            model.addAttribute("member", loginMember);
+        }
+
+        // fragment로 하자니 생각해보니 모든 페이지마다 th:block th:if 분기로 만들기엔 코드가 복잡해짐.
+        // layout 적용이 좋을듯
+
         return "home";
     }
 }
