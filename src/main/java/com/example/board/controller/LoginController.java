8031320b84660c94228fe6743149aef94b6e0848
LSM <dltmdals6718@naver.com>
2023-09-28 15:32:25 +0900

    Home Page 레이아웃 적용

--- src/main/java/com/example/board/controller/LoginController.java
+++ src/main/java/com/example/board/controller/LoginController.java
@@ -3,11 +3,12 @@ package com.example.board.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-    @PostMapping("/logout")
+    @GetMapping("/logout")
     public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if(session!=null) {
