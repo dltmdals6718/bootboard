723e26c9bc5dfa3bf2331a5f272d3a043bbbd6a5
LSM <dltmdals6718@naver.com>
2023-09-30 19:27:38 +0900

    타임리프 세션으로 로그인 여부에 따라 헤더 달리하기

--- src/main/java/com/example/board/controller/HomeController.java
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -12,19 +12,7 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember, Model model) {
-
-        if (loginMember == null) {
-            model.addAttribute("isLogin", false);
-        }
-        else {
-            model.addAttribute("isLogin", true);
-            model.addAttribute("member", loginMember);
-        }
-
-        // fragment로 하자니 생각해보니 모든 페이지마다 th:block th:if 분기로 만들기엔 코드가 복잡해짐.
-        // layout 적용이 좋을듯
-
+    public String home() {
         return "home";
     }
 }
