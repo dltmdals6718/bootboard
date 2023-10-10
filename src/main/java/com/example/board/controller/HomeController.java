a468c4e0bbadb8fde0af91b85770aa99ba3efeba
LSM <dltmdals6718@naver.com>
2023-10-10 20:05:15 +0900

    서블릿 예외 처리 - 오류 페이지 제공

--- src/main/java/com/example/board/controller/HomeController.java
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -2,11 +2,14 @@ package com.example.board.controller;
 
 import com.example.board.domain.Member;
 import com.example.board.domain.SessionConst;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttribute;
+
+import java.io.IOException;
 
 @Controller
 public class HomeController {
@@ -14,5 +17,20 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/error-ex")
+    public void errorEx() {
+        throw new RuntimeException("예외 발생");
+    }
+
+    @GetMapping("/error-404")
+    public void error404(HttpServletResponse response) throws IOException {
+        response.sendError(404, "404 오류.!");
+    }
+
+    @GetMapping("/error-500")
+    public void error500(HttpServletResponse response) throws IOException {
+        response.sendError(500);
     }
 }
