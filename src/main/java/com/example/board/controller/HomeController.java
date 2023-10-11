abe22f805b1db586875de248fd0770205f411aba
LSM <dltmdals6718@naver.com>
2023-10-11 20:11:35 +0900

    비회원이 게시글 작성시 메시지 띄우고 Redirect 시키기

--- src/main/java/com/example/board/controller/HomeController.java
+++ src/main/java/com/example/board/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.example.board.controller;
 
 import com.example.board.domain.Member;
 import com.example.board.domain.SessionConst;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import java.io.IOException;
@@ -15,7 +17,11 @@ import java.io.IOException;
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        // 세션 키 하나로 메시지, 멤버 다 출력 가능함!
+        // Object member = request.getSession(true).getAttribute(SessionConst.LOGIN_MEMBER);
+        // System.out.println("Member = " + member);
+        //Object attribute = request.getSession(true).getAttribute("msg");
         return "home";
     }
 
--- src/main/java/com/example/board/interceptor/LoginCheckInterceptor.java
+++ src/main/java/com/example/board/interceptor/LoginCheckInterceptor.java
@@ -1,12 +1,14 @@
 package com.example.board.interceptor;
 
 import com.example.board.domain.SessionConst;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+import java.io.PrintWriter;
 
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
@@ -18,7 +20,18 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession(false);
         if(session==null||session.getAttribute(SessionConst.LOGIN_MEMBER)==null) {
             log.info("미인증 사용자 요청");
+
+            //방법1) 메시지창 띄우고 redirect
+            response.setContentType("text/html; charset=utf-8");
+            PrintWriter writer = response.getWriter();
+            writer.println("<script>alert('로그인이 필요합니다.'); location.href='/?redirectURL=" + requestURI +"'</script>");
+
+            /*
+            방법2) 세션으로 메시지 관리
+            request.getSession(true).setAttribute("msg", "메시지");
             response.sendRedirect("/?redirectURL="+requestURI);
+            */
+
             return false;
         }
         return true;
